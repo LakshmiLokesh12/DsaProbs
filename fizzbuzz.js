5b6f20d58a3c44790a9beb5d1b7ec7873ef9806a
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-11 23:58:45 +0530

    basic prog done

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+
+//FIZZBUZZ PRGM (EXAM)
+/*let num=15;
+if(num%3==0)
+    console.log('Fizz');
+     if(num%5==0)
+    console.log('Buzz');
+ if(num%3==0 && num%5==0)
+console.log('FizzBuzz');
+
+*/
+
+//DIVISIBLE BY 2 PRGM (EXAM)
+/*let num=5;
+if(num%2==0)
+console.log("given number is divisible by 2");
+else
+console.log("given number is not divisible by 2");
+*/
+
