7a94d7bc01fd54225545175ce476a88bd8ea9328
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-13 21:33:49 +0530

    palindrome or not if no can count howmany changes needed to make palindrome

--- stringAlgorithm/palindrome.js
+++ stringAlgorithm/palindrome.js
@@ -11,4 +11,50 @@ function ispalindrome(s){
     return true;
    // console.log("true");
 }
-ispalindrome("abbac");
+//ispalindrome("abbac");
+
+
+
+let s = "ABACDABA";
+let cnt = 0;
+let flag = 0;
+while (s.length > 0){   
+    if (ispalindrome(s)) {
+        flag = 1;
+        break;
+    }
+    else {
+        cnt++;
+        s = s.substring(0, s.length - 1);
+        }
+}
+if (flag)
+console.log(cnt);
+
+  
+   function canConverttoPalindrome(s) {
+      let i = 0, j = s.length- 1;
+      
+      while (i < j && s.charAt(i) == s.charAt(j)) {
+        i++; j--;
+      }
+      
+      return i >= j ||
+             isPalindrome(s.charAt(j) + s.substring(i, j + 1)) ||
+             isPalindrome(s.substring(i, j + 1) + s.charAt(i));
+             console.log(s);
+    }
+    function isPalindrome(s){
+      let i = 0, j = s.length- 1;
+      
+      while (i < j && s.charAt(i) == s.charAt(j)) { 
+        i++; j--; 
+        
+      }
+      console.log(s);
+      return i >= j;
+    }
+    
+    //let s = "aacecaaa";
+  
+    canConverttoPalindrome(s);
