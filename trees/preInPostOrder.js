2ad873a5110a93405f66b797be98964b6d5641a0
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-15 23:01:51 +0530

    preordertraversal done

---
+++ trees/preInPostOrder.js
@@ -0,0 +1,39 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function preorderTraversal(root){
+    if(root == null) return; //base case
+    //root, left , right
+    console.log(root.data);
+    preorderTraversal(root.left);
+    preorderTraversal(root.right);
+}
+
+// function postorderTraversal(root){
+//     if(root == null) return; //base case
+//     //left , right, root
+//     postorderTraversal(root.left);
+//     postorderTraversal(root.right);
+//     console.log(root.data);
+// }
+
+// function inorderTraversal(root){
+//     if(root == null) return; //base case
+//     // left ,root, right
+//     inorderTraversal(root.left);
+//     console.log(root.data);
+//     inorderTraversal(root.right);
+// }
+
+let root = new Node(10);
+root.left = new Node(5);
+root.right = new Node(15);
+root.left.left = new Node(4);
+root.left.right = new Node(6);
+root.right.right = new Node(19);
+preorderTraversal(root);
