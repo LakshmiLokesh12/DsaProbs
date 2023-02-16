7279e66d7817b3865bfcd4c706c8202b748e2ce9
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-16 21:49:36 +0530

    height of tree is done

--- trees/preInPostOrder.js
+++ trees/preInPostOrder.js
@@ -36,8 +36,27 @@ root.right = new Node(15);
 root.left.left = new Node(4);
 root.left.right = new Node(6);
 root.right.right = new Node(19);
+root.right.left = new Node(24);
+root.left.left.left = new Node(30);
 preorderTraversal(root);
 console.log("----------------");
 postorderTraversal(root);
 console.log("--------------------");
 inorderTraversal(root);
+//console.log(root);
+console.log("---------");
+//heightOfTree(root);
+
+function heightOfTree(root){
+    if(root == null){
+        return -1;
+    }
+
+    let leftSubtreeHeight = heightOfTree(root.left); //-1 //0
+    let rightSubtreeHeight = heightOfTree(root.right);//-1 //0
+
+    let currHeight = Math.max(leftSubtreeHeight, rightSubtreeHeight) + 1;
+    return currHeight; //0
+}
+let height = heightOfTree(root);
+console.log(height);
