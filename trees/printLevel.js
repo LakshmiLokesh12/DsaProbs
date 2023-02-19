51e69108ee4067bddd63d0d9ad5c70b1018a893b
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-19 23:01:40 +0530

    print all levels of a binary tree done

--- trees/printLevel.js
+++ trees/printLevel.js
@@ -27,3 +27,25 @@ root.left.left = new Node(4);
 root.left.right = new Node(6);
 root.right.right = new Node(19);
 printLevelK(root, 0, 2);
+
+function heightOfTree(root){
+    if(root == null){
+        return -1;
+    }
+
+    let leftSubtreeHeight = heightOfTree(root.left); //-1 //0
+    let rightSubtreeHeight = heightOfTree(root.right);//-1 //0
+
+    let currHeight = Math.max(leftSubtreeHeight, rightSubtreeHeight) + 1;
+    return currHeight; //0
+}
+
+function printAllLevels(root){ //n + (n*height)=> O(n*height)
+    let lastLevel = heightOfTree(root);  //O(n)
+    for(let x = 0; x <= lastLevel; x++){ //O(n*height)
+        console.log("-------level : ",x," ------");
+        printLevelK(root, 0, x); //O(n)
+        console.log("-------------");
+    }
+}
+printAllLevels(root);
