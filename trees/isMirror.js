94fb763815b07d7e6ec42d0e2730e12f6e9d158e
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-22 23:49:53 +0530

    is mirror done

---
+++ trees/isMirror.js
@@ -0,0 +1,9 @@
+function isMirror(root){
+    return isMirrorHelper(root.left, root.right);
+}
+
+function isMirrorHelper(x, y){
+    if(x == null && y== null) return true; //both are null
+    if(x == null || y== null) return false; //any one of them is null
+    return (x.data == y.data) && isMirrorHelper(x.left, y.right) && isMirrorHelper(x.right, y.left);//both are not null
+}
