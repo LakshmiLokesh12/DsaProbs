e0195a0e093fad00d654a3df45f6f165855078bc
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-02-28 22:56:06 +0530

    done

--- trees/isMirror.js
+++ trees/isMirror.js
@@ -21,3 +21,52 @@ function isMirrorHelper(x, y){
     if(x == null || y== null) return false; //any one of them is null
     return (x.data == y.data) && isMirrorHelper(x.left, y.right) && isMirrorHelper(x.right, y.left);//both are not null
 }
+
+function printAllNodesAtDistanceK(root, k, x){
+
+    if(root == null){
+        return -1;
+    }
+
+    if(root.data == x){
+         printAllDescendentNodesAtDistanceK(root,k);
+         return 0;
+    }
+
+    let distanceLeft =  printAllNodesAtDistanceK(root.left, k, x);
+
+    if(distanceLeft != -1){ //node x exists on left
+        if(distanceLeft + 1 == k){ //root's distance from x
+            console.log(root.data);
+        }
+        else{
+            printAllDescendentNodesAtDistanceK(root.right, k-distanceLeft-2);
+        }
+    }
+
+    let distanceRight =  printAllNodesAtDistanceK(root.right, k, x);
+    if(distanceRight != -1){ //node x exists on right
+        if(distanceRight + 1 == k){//root's distance from x
+            console.log(root.data);
+        }
+        else{
+            printAllDescendentNodesAtDistanceK(root.left, k-distanceRight-2);
+        }
+    }
+
+}
+
+function printAllDescendentNodesAtDistanceK(root, k){
+    if(root == null || k<0){
+        return;
+    }
+    if(k == 0){
+        console.log(root.data);
+    }
+
+    printAllDescendentNodesAtDistanceK(root.left, k-1);
+    printAllDescendentNodesAtDistanceK(root.right, k-1);
+
+}
+
+
