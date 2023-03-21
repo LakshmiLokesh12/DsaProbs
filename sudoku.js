d7603f3527edf38f506962ee25f0eb8915bea122
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-03-21 14:47:31 +0530

    done

--- sudoku.js
+++ sudoku.js
@@ -1,54 +1,54 @@
-// let grid = [
-//     [3, 0, 6, 5, 0, 8, 4, 0, 0], 
-//     [5, 2, 0, 0, 0, 0, 0, 0, 0], 
-//     [0, 8, 7, 0, 0, 0, 0, 3, 1], 
-//     [0, 0, 3, 0, 1, 0, 0, 8, 0], 
-//     [9, 0, 0, 8, 6, 3, 0, 0, 5], 
-//     [0, 5, 0, 0, 9, 0, 6, 0, 0], 
-//     [1, 3, 0, 0, 0, 0, 2, 5, 0], 
-//     [0, 0, 0, 0, 0, 0, 0, 7, 4], 
-//     [0, 0, 5, 2, 0, 6, 3, 0, 0] 
-// ];
+let grid = [
+    [3, 0, 6, 5, 0, 8, 4, 0, 0], 
+    [5, 2, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 8, 7, 0, 0, 0, 0, 3, 1], 
+    [0, 0, 3, 0, 1, 0, 0, 8, 0], 
+    [9, 0, 0, 8, 6, 3, 0, 0, 5], 
+    [0, 5, 0, 0, 9, 0, 6, 0, 0], 
+    [1, 3, 0, 0, 0, 0, 2, 5, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 7, 4], 
+    [0, 0, 5, 2, 0, 6, 3, 0, 0] 
+];
 
 
-// function print(grid){
-//     let result = "";
-//     for(let i=0; i<= 8; i++){
-//         for(let j=0; j<=8; j++){
-//             result = result+grid[i][j] + " | ";
-//         }
-//         result += '\n';
-//     }
-//     console.log(result);
-// }
+function print(grid){
+    let result = "";
+    for(let i=0; i<= 8; i++){
+        for(let j=0; j<=8; j++){
+            result = result+grid[i][j] + " | ";
+        }
+        result += '\n';
+    }
+    console.log(result);
+}
 
 
-// function isSafe(i, j, num){
-//     //for same column
-//     for(let r = 0; r <= 8; r++){
-//         if(grid[r][j] == num){
-//             return false;
-//         }
-//     }
-//     //for same row
-//     for(let c = 0; c <= 8; c++){
-//         if(grid[i][c] == num){
-//             return false;
-//         }
-//     }
-//     //for same smaller box // 5/3 = 1.66   Math.floor(5/3) = 1
-//     let startRow = (Math.floor(i/3))*3;
-//     let startCol = (Math.floor(j/3))*3;
-//     for(let r = 0; r<=2; r++){
-//         for(let c = 0; c<=2; c++){
-//             if(grid[startRow+r][startCol+c] == num){
-//                 return false;
-//             }
-//         }
-//     }
+function isSafe(i, j, num){
+    //for same column
+    for(let r = 0; r <= 8; r++){
+        if(grid[r][j] == num){
+            return false;
+        }
+    }
+    //for same row
+    for(let c = 0; c <= 8; c++){
+        if(grid[i][c] == num){
+            return false;
+        }
+    }
+    //for same smaller box // 5/3 = 1.66   Math.floor(5/3) = 1
+    let startRow = (Math.floor(i/3))*3;
+    let startCol = (Math.floor(j/3))*3;
+    for(let r = 0; r<=2; r++){
+        for(let c = 0; c<=2; c++){
+            if(grid[startRow+r][startCol+c] == num){
+                return false;
+            }
+        }
+    }
 
-//     return true;
-// }
+    return true;
+}
 
 
 function solve(i, j){
